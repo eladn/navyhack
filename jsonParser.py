d263e977cd27b700efb96fde33a32bdead36b4a4
Omer <Omertorrents@gmail.com>
2016-11-28 20:08:27 +0200

    changed for RDS exec

--- jsonParser.py
+++ jsonParser.py
@@ -18,37 +18,43 @@ lastUpdate = {}
 decoder = lambda t:t.decode()
 
 
-def addIntoDb(mmsi, values,f):
+def addIntoDb(mmsi, values):
     values = list(map(decoder, values))
-    query = "ADD INTO ShipTracks(mmsi,lat,long,speed,course,time) VALUES(%s, %s, %s, %s, %s, %s);" \
+    sValues = "(%s, %s, %s, %s, %s, %s);" \
             %(mmsi, values[LAT], values[LONG], values[SPEED], values[COURSE], values[UPDATE_TIME])
-
-    f.write(query)
-    f.write("\n")
-def query():
+    return sValues
+def query(db):
     url = urlopen("http://shipfinder.co/endpoints/shipDeltaUpdate.php")
-
     if(url.getcode() is 200):
         j = json.loads(url.read())
         ships = j['ships']
-        with open("parser.txt", "w") as f:
-            for mmsi, vals in ships.items():
-                if(mmsi in lastUpdate) and (lastUpdate[mmsi] is vals[UPDATE_TIME].decode()):
-                    print "Ship number %s didnt change " %mmsi
-                    continue
-                lastUpdate[mmsi.decode()] = vals[UPDATE_TIME].decode()
-                addIntoDb(mmsi.decode(), vals, f    )
+        st = "ADD INTO ShipTracks(mmsi,lat,long,speed,course,time) VALUES"
+        for mmsi, vals in ships.items():
+            if(mmsi in lastUpdate) and (lastUpdate[mmsi] is vals[UPDATE_TIME].decode()):
+                continue
+            lastUpdate[mmsi.decode()] = vals[UPDATE_TIME].decode()
+            st += addIntoDb(mmsi.decode(), vals)+","
+        st = st[:-1]
+        db.execute(st+";")
+        db.commit()
+def init(): #TODO
+    # select mmsi, update from table parse it into lastUpdate[mmsi] = update
+    pass
 
 
 if __name__ == "__main__":
-    MySQLdb.connect(cnx['HOST'], cnx['USER'], cnx['PASSWORD'], cnx['db'], charset='utf8', use_unicode=True)
-    print 'success'
+    try:
+        db = MySQLdb.connect(cnx['HOST'], cnx['USER'], cnx['PASSWORD'], cnx['db'], charset='utf8', use_unicode=True)
+        print 'success'
+        i = 0
+        init()
+        while True:
+            print("running loop number %s" % i)
+            query(db)
+            time.sleep(5)
+            i += 1
+    except Exception:
+        print 'cant connect to elad`s db, blame him!#!'
+        exit()
 
 
-
-    i = 0
-    while True:
-        print("running loop number %s" %i)
-        query()
-        time.sleep(5)
-        i+=1
