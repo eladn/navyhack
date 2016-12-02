692e89879f0617e70d12be675f1c31cf1dfdbffa
Elad Nachmias <eladn@pazam.net>
2016-12-03 01:42:40 +0200

    main algo remarks & refactor

--- brain.py
+++ brain.py
@@ -16,6 +16,11 @@ from dist2coast import *
 from sklearn.linear_model import Lasso
 from sklearn.model_selection import cross_val_score, cross_val_predict
 from datetime  import timedelta
+import pickle
+from pickleloader import loadFile
+from math import pow, tan
+
+epsilon = 0.00001
 
 
 '''
@@ -81,43 +86,82 @@ def speedAndCourse(points,
     return [kmeans, X, clusters]
 
 def createAlgoVector(points):
-    # d = get_dist2coast_dict()
-    x = lambda y:True if y[7] is not "" and len(y[4])>50 and len(y[2])>50 else False
+    print('start load dist2coast')
+    with open('data/dist2coast.pkl', 'rb') as f:
+        dist2coast_lst = pickle.load(f)
+    print('end load dist2coast')
+
+    filterVector = lambda y:True if y[7] is not "" and len(y[4])>50 and len(y[2])>50 and y[7] != 'Other' else False
+
     def deriv(l,t):
-        return [(l[i] - l[i+1])/float((t[i]-t[i+1])/timedelta(seconds=1)) for i in range(len(l)-1)]
-    def courseToDCourse(listush):
-        l = []
-        for i in range(len(listush)-1):
-            l.append((listush[i]-listush[i+1])%360)
-        return l
+        return [((l[i] - l[i+1])/((t[i]-t[i+1])/timedelta(minutes=1))) for i in range(len(l)-1)]
+    def gradient(x,y,t):
+        return zip(deriv(x,t), deriv(y,t))  # list of pairs. each pair is the two partial-derivatives.
+    def derivative_direction(x,y,t):
+        return [0 if y < epsilon else tan(x/y) for x,y in gradient(x,y,t)]
+    def derivative_magnitude(x,y,t):
+        return [pow(x,2)+pow(y,2) for x,y in gradient(x,y,t)]
+    def add_to_all(l, delta):
+        return [item+delta for item in l]
+    def courseToDeltaCourse(course_values):
+        return [(course_values[i]-course_values[i+1])%360 for i in range(len(course_values)-1)]
 
-    # deriv = lambda l,t:[(l[i] - l[i+1])/(t[i]-t[i+1])/timedelta(seconds=1) for i in range(len(l)-1)]
-    r = lambda vector: \
-                courseToDCourse(vector[4][:50])+\
-                \
-                vector[2][:50]\
-                \
-                +[statistics.variance(vector[4])]+\
-                [statistics.variance(vector[2])]\
-                +vector[0][:50]+\
-                vector[1][:50]
+    # TODO: make the function receive set of wanted features to calc.
+    def calcVector(vector):
+        return courseToDeltaCourse(vector[4][:50]) +\
+            vector[2][:50]+\
+            [statistics.variance(vector[4])]+\
+            [statistics.variance(vector[2])]+ \
+            add_to_all(vector[0][:50], -vector[0][0])+ \
+            add_to_all(vector[1][:50], -vector[1][0]) + \
+            [statistics.mean([dist2coast(dist2coast_lst, vector[0][idx], vector[1][idx]) for idx in range(0, len(vector[0])-1)])]
 
-    # +[dist2coast(d,vector[0][0],vector[1][0])]
+            # --------- some experimental examples: --------
+            # stats of velocity vector (magnitude+direction):
+            #   [statistics.mean(derivative_magnitude(vector[0][:49], vector[1][:49], vector[-1][:49]))]
+            #   [statistics.variance(derivative_direction(vector[0][:49], vector[1][:49], vector[-1][:49]))]
+            # distance to shore statics:
+            #   [dist2coast(dist2coast_lst, vector[0][idx], vector[1][idx]) for idx in range(0, 30)]+ \
+            #   [statistics.variance(deriv([dist2coast(dist2coast_lst, vector[0][idx], vector[1][idx]) for idx in range(0, 49)], vector[-1][:49]))]
+            # maximal acceleration:
+            #   [max(deriv(derivative_magnitude(vector[0], vector[1], vector[-1]), vector[-1]))]
+            # maximal angular acceleration:
+            #   [max(deriv(derivative_direction(vector[0], vector[1], vector[-1]), vector[-1]))]
+            # course values deltas:
+            #   courseToDCourse(vector[4][:50])
+            # course derivative:
+            #   [x%360 for x in deriv(vector[4][:50], vector[-1][:50])]
+
     vectors = []
     labels = []
+    mmsi = []   # in order to export the test group and predicted labels to pickle
     for vector in points:
-        if(x(vector)):
-            vectors.append(r(vector))
+        if(filterVector(vector)):
+            vectors.append(calcVector(vector))
             labels.append(vector[7])
-    return vectors, labels
+            mmsi.append(vector[6])
+    return vectors, labels, mmsi
 
-def runAlgo(vec,label):
+def runAlgo(vec,label,mmsi):
     clf = svm.LinearSVC()
     x1,x2,y1,y2 = train_test_split(vec, label, test_size=0.4, random_state=0)
+
+    # in order to export the test group and predicted labels to pickle
+    xx1, xx2, yy1, test_ships_mmsi = train_test_split(vec, mmsi, test_size=0.4, random_state=0)
+
+    # learn
     clf.fit(x1,y1)
+
     cvScore = cross_val_score(clf,x2,y2,cv=7)
     print("SVM Linear SVC %f"%clf.score(x2,y2))
     print("SVM Linear SVC with crossValidation", cvScore)
+
+    predicted_labels = clf.predict(x2)
+
+    # export the test group and predicted labels to pickle
+    data = {'test_ships_mmsi': test_ships_mmsi, 'predicted_labels': predicted_labels}
+    with open('test_results.pkl', 'wb') as f:
+        pickle.dump(data, f)
 
     # clf = Lasso()
     # x1, x2, y1, y2 = train_test_split(vec, label, test_size=0.4, random_state=0)
--- dummy.py
+++ dummy.py
@@ -3,13 +3,20 @@ from plotres import *
 from parsers import *
 from pickleloader import *
 p = collectFiles("")
-vec, labels = createAlgoVector(p)
-mapper = loadFile("vessel_type_to_ship_type.pkl")
-t = lambda x:mapper[x] if x in mapper else "Other"
-labels = list(map(t, labels))
-superD = {'Tanker':'Merchant', 'Base':'Base', 'Supply':'Merchant', 'Passenger':'Merchant', 'Army':'Army', 'Other':'Other', 'Speed':'Army', 'Cargo':'Merchant', 'Tow':'Small', 'Fishing':'Small', 'Container':'Merchant'}
-t = lambda x:superD[x]
-labels = list(map(t,labels))
+
+# db&pickle save vessel_type grabbed from myshiptracking. here we map it to our reduced set of types (we call it ship type
+vessels_type_to_ship_type_mapper_data = loadFile("vessel_type_to_ship_type.pkl")
+map_vessel_type_to_ship_type = lambda x: \
+    vessels_type_to_ship_type_mapper_data[x] if x in vessels_type_to_ship_type_mapper_data else "Other"
+for vector in p:
+    vector[7] = map_vessel_type_to_ship_type(vector[7])
+
+vec, labels, mmsi = createAlgoVector(p)
+
+# second mapping to types. so that we'll have only 4 types (including other)
+second_ship_type_mapping = {'Tanker':'Merchant', 'Base':'Base', 'Supply':'Merchant', 'Passenger':'Merchant', 'Army':'Army', 'Other':'Other', 'Speed':'Army', 'Cargo':'Merchant', 'Tow':'Small', 'Fishing':'Small', 'Container':'Merchant'}
+second_ship_type_mapper = lambda x: second_ship_type_mapping[x]
+labels = list(map(second_ship_type_mapper, labels))
 print(set(labels))
-# print(set(labels))
-runAlgo(vec,labels)
+
+runAlgo(vec,labels,mmsi)
